c9b03a26a6844fc84bf0e97f830e5be50ec9bf17
hicks123 <korir.isaiah@gmail.com>
2018-09-21 15:31:05 +0300

    updates when updating to heroku server

--- src/app/server.js
+++ src/app/server.js
@@ -4,9 +4,26 @@ const path = require('path');
 
 const app = express();
 
+const forceSSL = function () {
+  return function (req, res, next) {
+    if (req.headers['x-forwarded-proto'] !== 'https') {
+      return res.redirect(
+        ['https://', req.get('Host'), req.url].join('')
+      );
+    }
+    next();
+  }
+}
+// Instruct the app
+// to use the forceSSL
+// middleware
+app.use(forceSSL());
+
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist'));
-
+app.get('/*', function (req, res) {
+  res.sendFile(path.join(__dirname + '/dist/index.html'));
+});
 app.get('/*', function (req, res) {
 
 });
